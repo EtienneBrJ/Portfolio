fd397ce354b55dbf02c3be378b5dfd9867717821
EtienneBrJ <etiennebrxv@gmail.com>
2021-10-30 20:30:05 +0200

    First clean up

--- funcs.py
+++ funcs.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ Module regrouping all the main functions
 """
-from logging import error
 import config, json, requests
 from web3 import Web3
 from flask import request, flash, redirect
 from eth_typing.encoding import HexStr
-from pycoingecko import CoinGeckoAPI
 from datetime import datetime
-
-
 
 
 ws_provider = Web3.WebsocketProvider(config.MAINNET_WSS)
@@ -111,22 +107,9 @@ def checkIncomingReq(input_data):
     if block:
         return block
 
-def getEthInfosFromCG():
-    """ Use Coin Gecko API to get some infos on Eth """
-
-    cg = CoinGeckoAPI()
-    eth_infos = cg.get_price(ids='ethereum', vs_currencies='usd', include_market_cap=True, include_24hr_vol=True)
-    return eth_infos
-
-def getEthInfosFromES():
-    """ Use Etherscan API to get some infos on Eth """
-    es_dict = open('etherscan.json', )
-    es_dict = json.load(es_dict)
-    return es_dict
-
 def getEthInfosFromBC():
     """ Use Blockchair API to get some infos on Eth 
-        Currently not using it, but want to remove EtherScan for that """
+    """
     req = requests.get('https://api.blockchair.com/ethereum/stats')
     text_response = json.loads(req.text)
     data = text_response.get('data')
@@ -134,7 +117,7 @@ def getEthInfosFromBC():
 
 
 def getAllTxsFees(nBlock):
-    """ Call web3 to get the all txs fees of a block
+    """ Call web3 to get all txs fees of a block
     """
     count = 0
     block = w3.eth.get_block(nBlock)
